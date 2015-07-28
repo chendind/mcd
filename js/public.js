68b8622bdc61ee128b4bd9c14644b16cd48d78b1
chendind <364044816@qq.com>
2015-07-28 10:49:22 +0800

    打了个框架

---
+++ js/public.js
@@ -0,0 +1,21 @@
+$(function(){
+	var body = $('body'), liheight = 44 , subliheight = 35, speed = 300;
+
+	$(".menu>li>a").on('click',function(){
+		var li = $(this).parent('li');
+		var l = li.find("ul.submenu>li").length-0;
+		if(l){
+			if(li.hasClass('open')&&li.hasClass('set')){
+				li.stop(true).animate({"height":liheight}, speed).removeClass('open');
+			}
+			else{
+				li.stop(true).animate({"height":subliheight*l+liheight},speed).addClass('open');
+			}
+		}
+		li.addClass('set').siblings('li').removeClass('set');
+	})
+
+	$("#menutoggle").on('click',function(){
+		body.toggleClass('menuclose');
+	})
+})
