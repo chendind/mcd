1466a4edf946fc5f1b8f836c76db8c541fe45184
chendind <364044816@qq.com>
2015-07-29 16:50:58 +0800

    下午先提交下

--- js/public.js
+++ js/public.js
@@ -1,6 +1,12 @@
 $(function(){
-	var body = $('body'), liheight = 44 , subliheight = 35, speed = 300;
+	// 公关的页面alertpanel关闭
+	$(document).on('click',function(){
+		$("#myfavorite").removeClass('open');
+	})
 
+
+
+	var body = $('body'), liheight = 44 , subliheight = 35, speed = 300;
 	$(".menu>li>a").on('click',function(){
 		var li = $(this).parent('li');
 		var l = li.find("ul.submenu>li").length-0;
@@ -18,12 +24,37 @@ $(function(){
 	$("#menutoggle").on('click',function(){
 		body.toggleClass('menuclose');
 	})
-	var linknum = $(".alertpanel .linkitem").length;
-	$.each($(".alertpanel .linkitem"),function(index){
-		if(index != 0 && index%5 == 0 && index != linknum){
-			// $(this).after("</div><div class=\"linkbox\">");
-			$(this).after("</div>");
-
+	var linknum = $(".alertpanel .linkitem").length-0, boxnum = Math.ceil((linknum+1)/6),
+	    box = $("<div class=\"linkbox\"></div>"), 
+	    tail = $('<a class="addbtn">Add New Link</a><div class="clear"></div>');
+	$(".alertpanel").width(boxnum*160+30);
+	for(i=0;i<boxnum;i++){
+		var thisbox = box.clone().appendTo($(".alertpanel"));
+		$.each($(".alertpanel>.linkitem"),function(index){
+			if(index<6){
+				console.log($(this).text());
+	    		$(this).appendTo(thisbox);
+			}
+		})
+		if(i+1==boxnum){
+			if(thisbox.children('.linkitem').length>4){
+				box.clone().appendTo($(".alertpanel"));
+			}
+			tail.appendTo($('.alertpanel'));
 		}
+	}
+	$("#myfavorite").on('click',function(e){
+		e.stopPropagation();
+		$(this).toggleClass("open");
+
 	})
+
+	$(".alertpanel").on('click',function(e){
+		e.stopPropagation();
+	})
+	// $.each($(".alertpanel .linkitem"),function(index){
+	// 	if(index != 0 && index%5 == 0 && index != linknum){
+	// 		$(this).after("</div><div class=\"linkbox\">");
+	// 	}
+	// })
 })
